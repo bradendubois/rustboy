5a353db479652c249a17bd63f25500865ed5c8b9
Braden Dubois <braden.dubois@usask.ca>
2021-05-05 21:41:35 -0600

    z80 mockup suggestion

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod gpu;
 mod gameboy;
 mod input;
 mod mmu;
+
 fn main() {
     println!("Hello, world!");
 }
--- src/z80/mod.rs
+++ src/z80/mod.rs
@@ -3,7 +3,8 @@ struct Clock {
     t: u16
 }
 
-struct Registers{
+
+struct Registers {
     a: u8,
     b: u8,
     c: u8,
@@ -16,4 +17,74 @@ struct Registers{
     sp: u16,
     m: u16,
     t: u16,
+}
+
+
+// Suggestion: Maybe this can come in handy
+type Opcode = u8;
+
+
+// Struct representing the Z80 CPU
+struct Z80 {
+
+    // Struct of all registers in the Z80
+    registers: Registers,
+
+    // Struct representing the clock of the Z80 for purposes of timing
+    clock: Clock,
+
+}
+
+
+impl Z80 {
+
+    // Initialization / creation of a Z80 CPU
+    fn init() -> Z80 {
+
+        Z80 {
+
+            // All registers begin empty
+            registers: Registers {
+                a: 0,
+                b: 0,
+                c: 0,
+                d: 0,
+                e: 0,
+                h: 0,
+                l: 0,
+                f: 0,
+                pc: 0,
+                sp: 0,
+                m: 0,
+                t: 0
+            },
+
+            // Clock begins at 0
+            clock: Clock { m: 0, t: 0 }
+        }
+    }
+
+
+    // Basic execution of the current operation at the program-counter (PC) register
+    fn step(&mut self) {
+
+        // Test variable to test the match statement
+        let x = 3;
+
+        /* Basic match of opcode -> function execution, catch-all at end will be to
+         * panic on any unmapped opcode execution as this should (probably) never occur
+         * */
+        match x {
+            0x00 => self.nop(),
+            _ => panic!("execution of unmapped opcode: {}", x)
+        };
+    }
+
+
+    // 0x00 - NOP ; no operation
+    fn nop (&mut self) {
+        self.registers.pc += 1;
+    }
+
+
 }
