1f29aeb4a6f70dbfbcfe6954a78da4c307a6c5f8
Braden Dubois <braden.dubois@usask.ca>
2021-05-06 15:15:19 -0600

    add 0x20 - jr nz s8

--- src/z80/mod.rs
+++ src/z80/mod.rs
@@ -50,6 +50,105 @@ pub struct Z80 {
 }
 
 
+// Struct representing one instruction
+struct Opcode {
+    size: u16,                      // size in bytes of the opcode; should be 1, 2, 3, no larger
+    clock_timing: Clock,            // the timing of m and t cycles taken in one instruction
+    instruction: fn(&mut Z80)       // the actual function that will
+}
+
+impl Opcode {
+
+    pub fn lookup(code: u8) -> Opcode {
+        match code {
+            0x00 => Opcode::nop(),
+            0x01 => Opcode::ld_bc(),
+            0x02 => Opcode::ld_bc_a(),
+            _ => panic!("Unmapped opcode {}", code)
+        }
+    }
+
+    // 0x00 - NOP
+    fn nop() -> Opcode {
+        Opcode {
+            size: 1,
+            clock_timing: Clock {
+                m: 1,
+                t: 4
+            },
+            instruction: |_cpu: &mut Z80| { }
+        }
+    }
+
+    // 0x01 - LD BC, d16
+    fn ld_bc() -> Opcode {
+        Opcode {
+            size: 1,
+            clock_timing: Clock {
+                m: 3,
+                t: 12
+            },
+            instruction: |cpu: &mut Z80| {
+                cpu.registers.c = cpu.mmu.read(cpu.registers.pc + 1);
+                cpu.registers.b = cpu.mmu.read(cpu.registers.pc + 2);
+            }
+        }
+    }
+
+    // 0x02 - LD (BC), A
+    fn ld_bc_a() -> Opcode {
+        Opcode {
+            size: 1,
+            clock_timing: Clock {
+                m: 3,
+                t: 12
+            },
+            instruction: |cpu: &mut Z80| {
+                cpu.mmu.write(cpu.registers.a, ((cpu.registers.b << 8) + cpu.registers.c).into());
+            }
+        }
+    }
+
+    // 0x03 - INC BC
+    fn inc_bc() -> Opcode {
+        Opcode {
+            size: 1,
+            clock_timing: Clock {
+                m: 2,
+                t: 8
+            },
+            instruction: |cpu: &mut Z80| {
+                cpu.registers.c += 1;
+                if cpu.registers.c == 0 {
+                    cpu.registers.b += 1;
+                }
+            }
+        }
+    }
+
+
+    // 0x20 - JR NZ s8
+    fn jr_nz_s8() -> Opcode {
+        Opcode {
+            size: 0,    // Real: 2 bytes, but directly modified in instruction
+            clock_timing: Clock {
+                m: 3,
+                t: 2
+            },
+            instruction: |cpu: &mut Z80| {
+                match cpu.registers.f >> 7 {
+                    0 => cpu.registers.pc += 2,
+                    1 => cpu.registers.pc += cpu.mmu.read(cpu.registers.pc + 1) as i8,
+                    _ => panic!("Somehow, the single-bit Z flag was neither 0 nor 1.")
+                }
+            }
+        }
+    }
+
+
+}
+
+
 #[allow(dead_code)]
 impl Z80 {
 
