aa7fd40b6b41fc8e58f575733e1c9260bfee1d5a
Braden Dubois <braden.dubois@usask.ca>
2021-05-20 15:24:00 -0600

    flesh mbc0

--- src/mmu/mbc/mod.rs
+++ src/mmu/mbc/mod.rs
@@ -1,12 +1,16 @@
 
 pub trait MBC {
-    fn read_ram(&mut self, address: u16) -> u8;
+    fn read_ram(&self, address: u16) -> u8;
+    fn read_rom(&self, address: u16) -> u8;
+    fn write_ram(&mut self, value: u8, address: u16);
+    fn write_rom(&mut self, value: u8, address: u16);
 }
 
 
 pub struct MBC0 {
-    rom: Vec<u8>,
+    rom: Vec<u8>
 }
+
 
 impl MBC0 {
     pub fn new(data: Vec<u8>) -> MBC0 {
@@ -17,7 +21,19 @@ impl MBC0 {
 }
 
 impl MBC for MBC0 {
-    fn read_ram(&mut self, address: u16) -> u8 {
+    fn read_ram(&self, _address: u16) -> u8 {
+        0
+    }
+
+    fn read_rom(&self, address: u16) -> u8 {
         self.rom[address as usize]
+    }
+
+    fn write_ram(&mut self, _value: u8, _address: u16) {
+
+    }
+
+    fn write_rom(&mut self, _value: u8, _address: u16) {
+
     }
 }
