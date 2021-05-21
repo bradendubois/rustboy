9b5869be25ba01ea17edafa7bacd2ada7710c20a
Braden Dubois <braden.dubois@usask.ca>
2021-05-21 11:34:42 -0600

    add basic fields / new() to Cartridge

--- src/cartridge/mod.rs
+++ src/cartridge/mod.rs
@@ -2,20 +2,44 @@ pub struct Cartridge {
 
     rom: Vec<u8>,
 
-    entry_point: usize,
-    nintendo_logo: Vec<u8>,
-    title: String,
-    manufacturer_code: String,
-    cgb_flag: u8,
-    new_licensee_code: (String, String),
-    sgb_flag: u8,
-    cartidge_type: u8,
-    rom_size: u8,
-    ram_size: u8,
-    destination_code: u8,
-    old_licensee_code: u8,
-    mask_version_version_number: u8,
-    header_checksum: u8,
-    global_checksum: u16
+    // Meta-data extracted from above ROM
+    entry_point: usize,             // 0x0100 - 0x0103
+    nintendo_logo: Vec<u8>,         // 0x0104 - 0x0133
+    title: String,                  // 0x0134 - 0x0143
+    manufacturer_code: String,      // 0x013F - 0x0142
+    cgb_flag: u8,                   // 0x0143
+    new_licensee_code: u16,         // 0x0144 - 0x0145
+    sgb_flag: u8,                   // 0x0146
+    cartidge_type: u8,              // 0x0147
+    rom_size: u8,                   // 0x0148
+    ram_size: u8,                   // 0x0149
+    destination_code: u8,           // 0x014A
+    old_licensee_code: u8,          // 0x014B
+    mask_rom_version_number: u8,    // 0x014C
+    header_checksum: u8,            // 0x014D
+    global_checksum: u16            // 0x014E - 0x014F
+}
 
+impl Cartridge {
+
+    pub fn new(&mut data: Vec<u8>) -> Cartridge {
+        Cartridge {
+            rom: data,
+            entry_point: 0x0100,
+            nintendo_logo: data[0x0104..0x0134],
+            title: data[0x0134..0x0143],
+            manufacturer_code: data[0x013F..0x0143],
+            cgb_flag: data[0x0143],
+            new_licensee_code: data[0x0144..0x0146],
+            sgb_flag: data[0x0146],
+            cartidge_type: data[0x0147],
+            rom_size: data[0x0148],
+            ram_size: data[0x0149],
+            destination_code: data[0x014A],
+            old_licensee_code: data[0x014B],
+            mask_rom_version_number: data[0x014C],
+            header_checksum: data[0x014D],
+            global_checksum: data[0x014E..0x0150]
+        }
+    }
 }
