863d417b5334bcf4d378d1c469d23309be137cb5
Braden Dubois <braden.dubois@usask.ca>
2021-05-24 15:00:00 -0600

    added vram / oam

--- src/ppu/mod.rs
+++ src/ppu/mod.rs
@@ -1,3 +1,5 @@
+const V_RAM_SIZE: usize = 0x2000;
+const   OAM_SIZE: usize = 0x0100;
 
 enum Mode {
     Mode0,  // HBlank Period
@@ -7,22 +9,33 @@ enum Mode {
 
 
 pub struct PPU {
-    mode: Mode
+    mode: Mode,                 // PPU Mode
+    vram: [u8; V_RAM_SIZE],     // VRAM
+     oam: [u8; OAM_SIZE],       // OAM / Sprite Attribute Table
+    lcdc: u8                    // LCDC Register : LCD C(ontrol) Register
 }
 
 impl PPU {
 
     pub fn new() -> PPU {
         PPU {
-            mode: Mode::Mode0
+            mode: Mode::Mode0,
+            vram: [0; V_RAM_SIZE],
+            oam: [0; OAM_SIZE],
+            lcdc: 0,
         }
     }
 
-    pub fn read_vram(&mut self, _address: u16) -> u8 {
+    pub fn read_vram(&mut self, address: u16) -> u8 {
+        let _true_address = PPU::addr_into_vram_space(address);
         0
     }
 
-    pub fn write_vram(&mut self, _value: u8, _address: u16) {
+    pub fn write_vram(&mut self, _value: u8, address: u16) {
+        let _true_address = PPU::addr_into_vram_space(address);
+    }
 
+    fn addr_into_vram_space(address: u16) -> u16 {
+        address % 0x8000
     }
 }
