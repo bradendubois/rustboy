4e66b44a13b1f87456d926e31b5904e373484f53
Braden Dubois <braden.dubois@usask.ca>
2021-05-24 13:27:19 -0600

    add ppu stub

---
+++ src/ppu/mod.rs
@@ -0,0 +1,21 @@
+
+pub struct PPU {
+
+}
+
+impl PPU {
+
+    pub fn new() -> PPU {
+        PPU {
+
+        }
+    }
+
+    pub fn read_vram(&mut self, _address: u16) -> u8 {
+        0
+    }
+
+    pub fn write_vram(&mut self, _address: u16) {
+
+    }
+}
